7de4eb62497be00cb7445cb5ac7cdeacb5e7b872
JoaoGabriel-png <227243817+JoaoGabriel-png@users.noreply.github.com>
2025-11-03 20:54:11 -0300

    Atualizar o batalhaNaval.c
    tinha esquecido de colocar o código de colocar a borda de letras e números

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -6,8 +6,13 @@
 //modo aventureiro:
 //modo mestre:
 
+
 int main() {
+
+    char l[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+
     int h[10][10] = {0};
+    int i, j;
 
     //barcos
     for (int j = 4; j <= 6; j++) {
@@ -18,9 +23,26 @@ int main() {
         h[i][1] = 3;
     }
 
+
+    //Colocar as letras no topo
+    for (int h = 0; h < 10; h ++)
+    {
+        if (h <= 0)
+        {
+            printf("    ");
+        }
+
+        printf("%c ", l[h]);
+        if (h >= 9)
+                {
+                    printf("\n");
+                }
+    }
+
     //tabuleiro
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
+    for (i = 0; i < 10; i++) {
+        printf("%2d. ", i+1);
+        for (j = 0; j < 10; j++) {
             printf("%d ", h[i][j]);
         }
         printf("\n");
