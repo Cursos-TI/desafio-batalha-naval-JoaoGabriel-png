23f41d34ed449eb51cf5edbe7512c2d2b6f7a399
JoaoGabriel-png <227243817+JoaoGabriel-png@users.noreply.github.com>
2025-11-05 15:17:21 -0300

    Atualizar o batalhaNaval.c
    modo aventureiro feito

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -3,9 +3,8 @@
 //eu programo em um programa externo e depois passo para ca
 
 //modo iniciante: feito
-//modo aventureiro:
+//modo aventureiro: feito
 //modo mestre:
-
 
 int main() {
 
@@ -15,6 +14,7 @@ int main() {
     int i, j;
 
     //barcos
+    /*
     for (int j = 4; j <= 6; j++) {
         h[3][j] = 3;
     }
@@ -22,9 +22,21 @@ int main() {
     for (int i = 3; i <= 5; i++) {
         h[i][1] = 3;
     }
+    */
+
+    //diagonal do canto esquerdo ao direito
+    for (int i = 0; i < 10; i++) {
+        h[i][i] = 3;
+    }
+
+    //diagonal do canto direito ao esquerdo
+    for (int i = 0; i < 10; i++) {
+        h[i][9 - i] = 5; //mudei para ficar mais facil a visualização
+    }
 
 
     //Colocar as letras no topo
+    printf("=====Batalha Naval=====\n\n");
     for (int h = 0; h < 10; h ++)
     {
         if (h <= 0)
