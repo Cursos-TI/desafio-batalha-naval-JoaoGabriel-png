4c9a63aad00b8f8d2ade46291823244fdf88fb66
JoaoGabriel-png <227243817+JoaoGabriel-png@users.noreply.github.com>
2025-11-06 20:11:32 -0300

    Atualizar o batalhaNaval.c
    mestre feito

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,17 +4,19 @@
 
 //modo iniciante: feito
 //modo aventureiro: feito
-//modo mestre:
+//modo mestre: feito
+
 
 int main() {
 
     char l[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
 
     int h[10][10] = {0};
-    int i, j;
+    int m[5][5] = {0}, c[3][5] = {0}, o[5][5] = {0}; //as coisas do 'mestre' que esqueci o nome
+    int i, j, in_i, in_j;
 
-    //barcos
     /*
+    //barcos
     for (int j = 4; j <= 6; j++) {
         h[3][j] = 3;
     }
@@ -24,6 +26,35 @@ int main() {
     }
     */
 
+
+    //cruz
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (i == 2 || j == 2) {
+                h[i + 4][j + 4]= 5;
+            }
+        }
+    }
+
+    //cone
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (j >= 2 - i && j <= 2 + i) {
+                h[i][j + 5]= 1;
+            }
+        }
+    }
+
+    //octaedro
+    for (int i = 0; i < 5; i++) {
+        int d = i <= 2 ? i : 4 - i;
+        for (int j = 2 - d; j <= 2 + d; j++) {
+            h[i + 2][j]= 3;
+        }
+    }
+
+
+    /*
     //diagonal do canto esquerdo ao direito
     for (int i = 0; i < 10; i++) {
         h[i][i] = 3;
@@ -32,7 +63,7 @@ int main() {
     //diagonal do canto direito ao esquerdo
     for (int i = 0; i < 10; i++) {
         h[i][9 - i] = 5; //mudei para ficar mais facil a visualização
-    }
+    }*/
 
 
     //Colocar as letras no topo
